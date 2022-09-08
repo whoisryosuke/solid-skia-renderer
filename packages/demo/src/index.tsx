2ea1e6b10e574d0cc34e3c432e874c7ee3be0fe0
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-08 11:26:26 -0700

    :books: better explanations

--- packages/demo/src/index.tsx
+++ packages/demo/src/index.tsx
@@ -8,21 +8,23 @@ type ButtonProps = {
   position: number[];
 }
 
+// We can create functional components like we would in Solid/React/Preact
 const Button = ({position}: ButtonProps) => {
   console.log('[BUTTON] rendering')
 
   // In order to give the end user access to drawing to canvas
   // We return a class that extends VElement with a `render()` method
   // Any `<Window>` will go through it's child components and run that `render()`
+  // This is similar to the DOM's `document.createElement('div')`, which happens underneath your `<div>`
+  // @TODO: Allow this -- but also let user type `<button>` and get a generic button from Solid universal?
   return new SkiaButton(position);
 }
 
-// Signals work! Kinda.
+// Signals work! 
 const [frames, setFrames] = createSignal(0);
 
 const App = () => {
   // We can use state inside of components
-  const { bears } = store.getState();
 
   // But we can't do things like "loops"
   // We get this through the `render` method in our SkiaElement 
@@ -38,9 +40,18 @@ const App = () => {
   setFrames((prev) => prev + 1)
   console.log('[APP] rendering App component', frames());
 
+  // Any inline conditionals need to be memoized separately
+  // This is a SolidJS server issue - it reaches for web APIs
+  // Using the `createMemo` function directly circumvents this (albeit not as nice)
+  // **Ultimately, this only runs once, so any state/store stuff won't work**
+  const optionalButton = createMemo(() => { 
+    const { bears } = store.getState();
     //@ts-ignore
-  const optionalButton = createMemo(() => bears > 1 && <Button position={[10,10]} />)
+    return bears > 1 && <Button position={[100,100]} />
+  })
+
   return (
+    // @TODO: Add void and SkiaElement as a return to JSX Elements (in global.d.ts)
     //@ts-ignore
     <Window>
       {/* @ts-ignore */}
